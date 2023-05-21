5fce3b1cdb71376048f4cde400518104d90c63a0
Romário Arruda <romarioarruda98@gmail.com>
2023-05-21 15:05:27 -0300

    Verificando capacidade do Vetor

--- src/vetores.rs
+++ src/vetores.rs
@@ -3,7 +3,8 @@ fn main() {
     notas.push(10.0);
     notas.push(9.0);
     notas.push(8.3);
-
+    
+    println!("Capacidade do vetor: {}", notas.capacity());
     println!("{:?}", notas);
 
     let notas_v2: Vec<f32> = vec![6.4, 7.0, 8.2, 9.0, 10.0]; //vec! is a macro for Vec::new()
