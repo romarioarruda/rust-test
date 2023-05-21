ecc5b18f7aa66780e36502edb95f5c7b26d3ebb4
Romário Arruda <romarioarruda98@gmail.com>
2023-05-21 14:51:38 -0300

    Pegando valores do vetor com get

--- src/vetores.rs
+++ src/vetores.rs
@@ -9,4 +9,19 @@ fn main() {
     let notas_v2: Vec<f32> = vec![6.4, 7.0, 8.2, 9.0, 10.0]; //vec! is a macro for Vec::new()
 
     println!("{:?}", notas_v2);
+
+    println!("Nota 1: {}", match notas_v2.get(0) {
+        Some(n) => *n,
+        None => 0.0
+    });
+
+    println!("Nota 3: {}", match notas_v2.get(2) {
+        Some(n) => *n,
+        None => 0.0
+    });
+
+    println!("Nota 10: {}", match notas_v2.get(10) {
+        Some(n) => *n,
+        None => 0.0
+    })
 }
